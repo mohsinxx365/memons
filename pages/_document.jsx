05e5d71e3aa1acdf1376b9e4fc1c5a9d4f6242e5
Mohsin <48387331+mohsinxx365@users.noreply.github.com>
2020-07-05 06:38:39 +0300

    Initial commit

---
+++ pages/_document.jsx
@@ -0,0 +1,54 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Jost:wght@400;500;700&display=swap"
+            rel="stylesheet"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="favicons/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="favicons/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="favicons/favicon-16x16.png"
+          />
+          <link rel="manifest" href="favicons/site.webmanifest" />
+          <link
+            rel="mask-icon"
+            href="favicons/safari-pinned-tab.svg"
+            color="#00aaa8"
+          />
+          <meta name="apple-mobile-web-app-title" content="Memons" />
+          <meta name="application-name" content="Memons" />
+          <meta name="msapplication-TileColor" content="#00aaa8" />
+          <meta name="theme-color" content="#ffffff" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
