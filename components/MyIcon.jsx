9a95bec1a37c2757e76221492e49bafb01dd4ec9
Mohsin <48387331+mohsinxx365@users.noreply.github.com>
2020-07-12 14:29:30 +0300

    added social icon data !!!

--- components/MyIcon.jsx
+++ components/MyIcon.jsx
@@ -2,10 +2,12 @@ import { SvgIcon } from "@material-ui/core";
 import { icons } from "../utils/iconData";
 
 export default ({ icon }) => {
+  const iconData = icons[icon];
+
   return (
     <SvgIcon xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
-      <path d={icons[icon].path1} style={{ opacity: 0.4 }} />
-      <path d={icons[icon].path2} />
+      <path d={iconData.path1} style={{ opacity: 0.4 }} />
+      {iconData.path2 ? <path d={iconData.path2} /> : ""}
     </SvgIcon>
   );
 };
